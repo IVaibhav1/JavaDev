f33058728e4060f3f75169f600ff06b26a088f41
Vaibhav Agrahari <agraharivaibhav7@gmail.com>
2021-04-28 10:05:44 +0530

    Add files via upload

---
+++ Ticket.java
@@ -0,0 +1,24 @@
+package com.collections.hashmap.assignment;
+
+ public class Ticket {
+	 int fare;
+
+	
+
+	public int getFare() {
+		return fare;
+	}
+
+	public void setFare(int fare) {
+		this.fare = fare;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+
+}
